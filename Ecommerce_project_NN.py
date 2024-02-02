d692d146b62ea869e5f259de1f1200176772d196
Jakub Krys <jakkrys@gmail.com>
2024-02-03 00:39:17 +0100

    ECommerce project

---
+++ Ecommerce_project_NN.py
@@ -0,0 +1,60 @@
+"""
+In logistic_softmax_train.py, it is shown how to use logistic regression (i.e. no hidden layers) for multiclass
+classification (softmax instead of the logistic function) on the ecommerce project data.
+Here, we will apply what we've learned in training_NNs.py to use a simple (1 hidden layer) NN on this data.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as sp
+from Training_NNs import derivative_W2, derivative_W1, derivative_b2, derivative_b1, feedforward, predict
+from process import get_data
+
+Xtrain, Ytrain, Xtest, Ytest = get_data()
+
+Ntrain, D = Xtrain.shape
+Ntest, _ = Xtest.shape
+M = 5
+K = len(set(Ytrain) | set(Ytest))  # the OR covers the case that some classes are not included in the test or train set
+
+Ttrain = np.zeros((Ntrain, K), dtype=int)
+Ttest = np.zeros((Ntest, K), dtype=int)
+
+for ii in range(Ntrain):
+    Ttrain[ii, Ytrain[ii]] = 1
+
+for ii in range(Ntest):
+    Ttest[ii, Ytest[ii]] = 1
+
+# randomly initialize weights
+W1 = np.random.randn(D, M)
+b1 = np.random.randn(M)
+W2 = np.random.randn(M, K)
+b2 = np.random.randn(K)
+
+cost = []
+classification_rate = []
+learning_rate = 1e-3
+steps = 10000
+
+
+for ii in range(steps):
+    Z, pY = feedforward(Xtrain, W1, b1, W2, b2)
+    if ii % 100 == 0:
+        preds = predict(pY)
+        classification_rate.append( np.mean(preds == Ytrain).round(2) )
+        cost.append(np.sum(Ttrain * np.log(pY)))
+
+    W1 += learning_rate * derivative_W1(Xtrain, Z, W2, pY, Ttrain)
+    b1 += learning_rate * derivative_b1(Z, W2, pY, Ttrain)
+    W2 += learning_rate * derivative_W2(Z, pY, Ttrain)
+    b2 += learning_rate * derivative_b2(pY, Ttrain)
+
+plt.plot(cost)
+plt.title('Cross-entropy loss in gradient descent')
+plt.show()
+
+print(f'Final classification rate for the train set: {classification_rate[-1]}')
+
+_, pYtest = feedforward(Xtest, W1, b1, W2, b2)
+print(f'Classification rate for the test set: {np.mean(predict(pYtest) == Ytest).round(2)}')
--- Training_NNs.py
+++ Training_NNs.py
@@ -7,30 +7,20 @@ We're going to use a simple NN with two layers.
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special as sp
-import backprop
 
-# this is the only thing I take from the course materials. The rest will be implemented on my own.
-# create the data
-Nclass = 500
-D = 2  # dimensionality of input
-M = 3  # hidden layer size
-K = 3  # number of classes
-
-X1 = np.random.randn(Nclass, D) + np.array([0, -2])
-X2 = np.random.randn(Nclass, D) + np.array([2, 2])
-X3 = np.random.randn(Nclass, D) + np.array([-2, 2])
-X = np.vstack([X1, X2, X3])
-
-labels = np.array([0] * Nclass + [1] * Nclass + [2] * Nclass)  # labels for the 3 classes
-N = len(labels)  # total number of samples
-
-plt.scatter(X[:, 0], X[:, 1], c=labels, s=100, alpha=0.5)
-plt.show()
-
-# create the indicator matrix for the targets
-T = np.zeros((N, K), dtype=int)
-for ii in range(N):
-    T[ii, labels[ii]] = 1
+# X - input matrix, size NxD
+# Z - hidden layer, size NxM
+# Y - output layer, size NxK
+# T - target one-hot encoded as an indicator matrix, size NxK
+# W2 - weight matrix of the second layer
+def derivative_W1(X, Z, W2, Y, T):
+    return X.T.dot((T - Y).dot(W2.T) * Z * (1 - Z))
+def derivative_b1(Z, W2, Y, T):
+    return np.sum((T - Y).dot(W2.T) * Z * (1 - Z), axis=0)
+def derivative_W2(Z, Y, T):
+    return Z.T.dot(T - Y)
+def derivative_b2(Y, T):
+    return np.sum(T - Y, axis=0)
 
 def feedforward(X, W1, b1, W2, b2):
     a = X.dot(W1) + b1
@@ -39,33 +29,59 @@ def feedforward(X, W1, b1, W2, b2):
     Y = sp.softmax(alpha, axis=1)
     return Z, Y
 
-# randomly initialize weights
-W1 = np.random.randn(D, M)
-b1 = np.random.randn(M)
-W2 = np.random.randn(M, K)
-b2 = np.random.randn(K)
+def predict(pY):
+    return np.argmax(pY, axis=1)
 
-cost = []
-learning_rate = 1e-3
-steps = 1000
+def main():
+    # this is the only thing I take from the course materials. The rest will be implemented on my own.
+    # create the data
+    Nclass = 500
+    D = 2  # dimensionality of input
+    M = 3  # hidden layer size
+    K = 3  # number of classes
 
-for ii in range(steps):
-    Z, Y = feedforward(X, W1, b1, W2, b2)
-    if ii % 100 == 0:
-        preds = np.argmax(Y, axis=1)
-        classification_rate = np.mean(preds == np.argmax(T)).round(2)
-        cost.append(np.sum(T * np.log(Y)))
+    X1 = np.random.randn(Nclass, D) + np.array([0, -2])
+    X2 = np.random.randn(Nclass, D) + np.array([2, 2])
+    X3 = np.random.randn(Nclass, D) + np.array([-2, 2])
+    X = np.vstack([X1, X2, X3])
 
-    dJdW1 = X.T.dot((T - Y).dot(W2.T) * Z * (1 - Z))
-    dJdb1 = np.sum((T - Y).dot(W2.T) * Z * (1 - Z), axis=0)
-    dJdW2 = Z.T.dot(T - Y)
-    dJdb2 = np.sum(T - Y, axis=0)
+    labels = np.array([0] * Nclass + [1] * Nclass + [2] * Nclass)  # labels for the 3 classes
+    N = len(labels)  # total number of samples
 
-    W1 += learning_rate * dJdW1
-    b1 += learning_rate * dJdb1
-    W2 += learning_rate * dJdW2
-    b2 += learning_rate * dJdb2
+    plt.scatter(X[:, 0], X[:, 1], c=labels, s=100, alpha=0.5)
+    plt.show()
 
-plt.plot(cost)
-plt.title('Cross-entropy loss in gradient descent')
-plt.show()
+    # create the indicator matrix for the targets
+    T = np.zeros((N, K), dtype=int)
+    for ii in range(N):
+        T[ii, labels[ii]] = 1
+
+    # randomly initialize weights
+    W1 = np.random.randn(D, M)
+    b1 = np.random.randn(M)
+    W2 = np.random.randn(M, K)
+    b2 = np.random.randn(K)
+
+    cost = []
+    learning_rate = 1e-3
+    steps = 1000
+
+    for ii in range(steps):
+        Z, Y = feedforward(X, W1, b1, W2, b2)
+        if ii % 100 == 0:
+            preds = predict(Y)
+            classification_rate = np.mean(preds == labels).round(2)
+            cost.append(np.sum(T * np.log(Y)))
+
+        W1 += learning_rate * derivative_W1(X, Z, W2, Y, T)
+        b1 += learning_rate * derivative_b1(Z, W2, Y, T)
+        W2 += learning_rate * derivative_W2(Z, Y, T)
+        b2 += learning_rate * derivative_b2(Y, T)
+
+    plt.plot(cost)
+    plt.title('Cross-entropy loss in gradient descent')
+    plt.show()
+    print(f'Final classification rate: {classification_rate}')
+
+if __name__ == '__main__':
+    main()
